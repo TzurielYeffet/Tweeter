d98c60bfd4527e657137269f15dfe571ad173028
TzurielYeffet <Tzuriel66ye@yahoo.com>
2025-07-28 19:33:36 +0300

    Step 3 finalization

---
+++ render.js
@@ -0,0 +1,46 @@
+class Renderer {
+  constructor() {}
+
+  renderPosts(posts) {
+    const postsTag = document.getElementById("posts");
+    postsTag.innerHTML = "";
+
+    posts.forEach((post) => {
+      const $post = $("<div>").addClass("post").attr("data-id", post.id);
+      const $postText = $("<div>").addClass("post-text").text(post.text);
+      const $deletePost = $("<div>")
+        .addClass("delete")
+        .attr("data-id", post.id)
+        .text("Delete Post");
+      const $comments = $("<div>").addClass("comments");
+      $post.append($postText, $comments);
+      $("#posts").append($post);
+      //   post.comments.forEach((comment) => {
+      //     const $comment = $("<div>")
+      //       .addClass("comment")
+      //       .attr("data-id", comment.id)
+      //       .text(comment.text);
+      //     const $deleteComment = $("<div>").addClass("delete-comment").text("X");
+      //     $comments.append($deleteComment,$comment );
+      //   });
+      post.comments.forEach((comment) => {
+        const $commentLine = $("<div>").addClass("comment-line"); // wrapper for alignment
+        const $comment = $("<div>")
+          .addClass("comment")
+          .attr("data-id", comment.id)
+          .text(comment.text);
+        const $deleteComment = $("<div>").addClass("delete-comment").text("X");
+        $commentLine.append($deleteComment, $comment);
+        $comments.append($commentLine);
+      });
+      const $postComment = $("<div>").addClass("post-comment");
+      const $input = $("<input>")
+        .addClass("comments-input")
+        .attr("placeholder", "Got something to say?");
+      const $button = $("<button>").addClass("comment-button").text("Comment");
+      $postComment.append($input, $button);
+      $post.append($postComment, $deletePost);
+    });
+  }
+}
+export default Renderer;
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import Tweeter from "./model.js";
-
+import Renderer from "./render.js";
 const tweeter = new Tweeter();
-
+const render= new Renderer();
 // Test adding a post
 tweeter.addPost("This is my own post!");
 console.log(tweeter.getPosts());
@@ -22,3 +22,27 @@ console.log(tweeter.getPosts());
 // Test removing comments
 tweeter.removeComment("p2", "c6");
 console.log(tweeter.getPosts());
+
+render.renderPosts([
+      {
+        text: "First post!",
+        id: "p1",
+        comments: [
+          { id: "c1", text: "First comment on first post!" },
+          { id: "c2", text: "Second comment on first post!!" },
+          { id: "c3", text: "Third comment on first post!!!" },
+        ],
+      },
+      {
+        text: "Aw man, I wanted to be first",
+        id: "p2",
+        comments: [
+          {
+            id: "c4",
+            text: "Don't worry second poster, you'll be first one day.",
+          },
+          { id: "c5", text: "Yeah, believe in yourself!" },
+          { id: "c6", text: "Haha second place what a joke." },
+        ],
+      },
+    ])
